4d0eaf0355055bdcae84224aefdc0f1480fa2237
nknathi <nknathindamase@gmail.com>
2023-09-25 06:26:25 +0200

    Quantifier component comments

--- shopCart/src/components/Products/Products.tsx
+++ shopCart/src/components/Products/Products.tsx
@@ -1,12 +1,20 @@
+// used in React to define functional components and manage component side effects and state.
 import { FunctionComponent, useEffect, useState } from "react";
+// used for managing local storage in your React application.
 import useLocalStorageState from "use-local-storage-state";
 
+// component or utility function named CurrencyFormatter
 import { CurrencyFormatter } from '../CurrencyFormatter';
+// import styles
 import classes from './products.module.scss';
+// component is used to display loading animations or indicators.
 import { Loader } from '../Loader/Loader';
 
+// stores the URL for an API endpoint
 const API_URL = 'https://dummyjson.com/products';
 
+// interface or type definition that describes the structure of a product object
+// includes properties like id, title, price, thumbnail, image, and quantity
 export type Product = {
     id: number,
     title: string,
@@ -20,16 +28,27 @@ export interface CartProps {
     [productId: string]: Product
 }
 
+// The Products component is defined as a functional component using React's FunctionComponent
 export const Products: FunctionComponent = () => {
+    // uses React state hooks (useState) to manage several pieces of state
+    // boolean state variable indicating whether data is currently being loaded.
     const [isLoading, setIsLoading] = useState(true);
+    // array of Product objects representing the list of products fetched from an API.
     const [products, setProducts] = useState<Product[]>([]);
+    // boolean state variable indicating whether an error occurred during data fetching.
     const [error, setError] = useState(false);
+    // state variable is managed using a custom hook called useLocalStorageState, and it represents the items in the shopping cart.
+    // It's initialized with an empty object {}.
     const [cart, setCart] = useLocalStorageState<CartProps>('cart', {});
 
+    // useEffect hook is used to fetch data from an API when the component mounts (i.e., when it is first rendered)
     useEffect(() => {
+        // uses the fetchData function to make an asynchronous request to an API_URL
         fetchData(API_URL);
     }, [])
 
+    // asynchronous function is responsible for fetching data from the specified URL.
+    // It uses fetch and handles both success and error scenarios by setting the products, isLoading, and error state accordingly.
     async function fetchData(url: string) {
         try {
             const response = await fetch(url);
@@ -49,7 +68,10 @@ export const Products: FunctionComponent = () => {
         }
     }
 
+    // function is called when the "Add to Cart" button is clicked.
+    // It takes a Product object as an argument and adds it to the cart state
     const addToCart = (product: Product):void => {
+        // sets the quantity of the product to 1 and updates the cart state with the new product.
         product.quantity = 1;
 
         setCart((prevCart) => ({
@@ -58,16 +80,23 @@ export const Products: FunctionComponent = () => {
         }));
     }
 
+    // function checks whether a product with a given productId is already in the cart. 
+    // It converts the cart object into an array of keys (product ids) and checks if the productId is included in that array.
     const isInCart = (productId: number):boolean => Object.keys(cart || {}).includes(productId.toString());
 
+    // If there is an error (error is true), it renders an error message.
     if(error) {
         return <h3 className={classes.error}>An error occurred when fetching data. Please check the API and try again.</h3>
     }
 
+    // If data is still loading (isLoading is true), it renders a loader component.
     if (isLoading) {
         return <Loader/>
     }
 
+    // it renders the list of products obtained from the API.
+    // It maps through the products array, rendering each product's details along with an "Add to Cart" button.
+    // The button is disabled if the product is already in the cart
     return (
         <section className={classes.product}>
             <h1>Products</h1>
--- shopCart/src/components/Quantifier/Quantifier.tsx
+++ shopCart/src/components/Quantifier/Quantifier.tsx
@@ -1,20 +1,35 @@
+// creating React functional components and managing component state
 import { FunctionComponent, useState  } from "react";
+// importing styles
 import classes from './quantifier.module.scss';
 
+// defines a custom type Operation, which can have two values: 'decrease' or 'increase'.
+// It is used to specify the operation to be performed when the user clicks the buttons.
 export type Operation = 'decrease' | 'increase';
 
+// defines the component's prop types
 interface Props {
+    // callback function that takes a productId as a parameter and is used to remove the product
     removeProductCallback: (productId: number) => void
+    // callback function that takes a productId and an operation (either 'decrease' or 'increase') as parameters and is used to update the product quantity.
     handleUpdateQuantity: (productId: number, operation: Operation) => void
+    // unique identifier for the product associated with this quantity input.
     productId: number
 }
 
+// functional component named Quantifier that takes the defined Props as its props.
+// Inside the component, the props are destructured to access removeProductCallback, handleUpdateQuantity, and productId.
 export const Quantifier: FunctionComponent<Props> = ({ removeProductCallback, handleUpdateQuantity, productId }) => {
+    // initializes a state variable value using the useState hook.
+    // It starts with a default value of 1 and is used to store the quantity.
     const [value, setValue] = useState<number>(1);
 
+    // defined as click handlers for the '-' 
     const reduce = ():void => {
+        // functions call handleUpdateQuantity with the productId and the corresponding operation 'decrease'
         handleUpdateQuantity(productId, 'decrease');
 
+        // update the value state accordingly.
         setValue(prevState => {
             const updateValue = prevState - 1;
 
@@ -25,16 +40,23 @@ export const Quantifier: FunctionComponent<Props> = ({ removeProductCallback, ha
         })
     }
 
+    // defined as click handlers for the '+' 
     const increase = ():void => {
+        // functions call handleUpdateQuantity with the productId and the corresponding operation 'increase'
         handleUpdateQuantity(productId, 'increase');
+        // update the value state accordingly.
         setValue(prevState => prevState + 1)
     }
 
+    // defines the JSX structure to render the quantity input and buttons
     return (
         <div className={classes.quantifier}>
+            // button to decrease the quantity
             <input type="button" value="-" className={classes.buttonMinus} onClick={reduce} />
+            // display and edit the quantity
             <input type="number" step="1" max="" value={value} onChange={e => setValue(parseInt(e.target.value))}
             className={classes.quantityField} />
+            // button to increase the quantity
             <input type="button" value="+" className={classes.buttonPlus} onClick={increase} />
         </div>
     )

82f1574e3b7997eba97def989e3b9023ef6c6278
nknathi <nknathindamase@gmail.com>
2023-09-25 05:58:12 +0200

    Loader component comments

--- shopCart/src/components/Loader/Loader.tsx
+++ shopCart/src/components/Loader/Loader.tsx
@@ -1,7 +1,11 @@
+// used to define a functional component's type.
 import { FunctionComponent } from 'react';
+// importing styles
 import classes from './loader.module.scss';
 
+// Loader is a functional component that takes no prop
 export const Loader:FunctionComponent = () => (
+    // it renders a <div> element with a class name of classes.loader. 
     <div className={classes.loader}>
         <div className={classes.spinner}></div>
     </div>
